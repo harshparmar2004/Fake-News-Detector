eb3fb73535d45dbef3742e999bf9b0a22838c000
harsh parmar <harshparmar686630@gmail.com>
2025-10-04 16:22:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyDfgXvEmOsfu4oLZEhfTk7SUEBnSiri-Ic),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyDfgXvEmOsfu4oLZEhfTk7SUEBnSiri-Ic)
       },
       resolve: {
         alias: {
